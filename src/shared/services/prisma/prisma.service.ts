50fc6621b5eda3b815954fac416de3cc28e2b8c6
Fernando <fernandocr019@gmail.com>
2026-03-16 19:53:01 -0300

    chore: initial project setup

---
+++ src/shared/services/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      this.logger.log('Banco de dados conectado com sucesso');
+    } catch (error) {
+      this.logger.error(
+        'Não foi possível conectar ao banco de dados',
+        error.message,
+      );
+      process.exit(1);
+    }
+  }
+}
